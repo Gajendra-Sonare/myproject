6bb2ab923fc99835a4e5413c3db787ed305a89ac
Gajendra-Sonare <60471843+Gajendra-Sonare@users.noreply.github.com>
2021-09-21 20:48:36 +0530

    Delete new/myproject directory

--- new/myproject/index/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-from numpy import mod
-
-# Create your models here.
-class stocks_model(models.Model):
-    name = models.CharField(max_length=10)
-
-    def __str__(self):
-        return self.name
--- new/myproject/index/serializers.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-from rest_framework import serializers
-from .models import stocks_model
-
-class stockserializer(serializers.ModelSerializer):
-    class Meta:
-        model = stocks_model
-        fields = '__all__'
