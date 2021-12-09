8c5ce7a85545e253f1158e7bcd11abcfe6c11dd4
Gajendra-Sonare <60471843+Gajendra-Sonare@users.noreply.github.com>
2021-12-09 19:06:47 +0530

    Update main.py

--- application/main.py
+++ application/main.py
@@ -3,11 +3,11 @@ from numpy.lib.utils import info
 from bs4 import BeautifulSoup
 from PIL import Image, ImageTk
 import plotly.express as ex
-from tkinter import messagebox
+from tkinter import Canvas, messagebox
 from tkinter.font import Font
 from tkinter import ttk
 from threading import *
-from tkinter import *
+import tkinter as tk
 import pandas as pd
 import requests
 import yfinance
@@ -15,6 +15,9 @@ import threading
 import json
 import sys
 import os
+
+from plotly.subplots import make_subplots
+import plotly.graph_objects as go
 
 def start():
     company = symbol_input.get("1.0",'end-1c')
@@ -34,7 +37,7 @@ def start():
                 txt = txt.split()
             else:
                 messagebox.showerror("Error","Cannot collect data at this time. Try again later.")
-                Canvas.delete(tab1,text_1)
+                tk.Canvas.delete(tab1,text_1)
                 tab1.create_text(20,20,anchor="nw",text="try again",font=("Arial,15"))
                 return
         
@@ -42,7 +45,13 @@ def start():
             data = yfinance.Ticker(company)
             inf = data.info
             df_inf = pd.DataFrame().from_dict(inf,orient='index')
-            desc = df_inf.iloc[3]
+            try:
+                desc = df_inf.iloc[3]
+            except:
+                messagebox.showerror("Error","Cannot collect data at this time. Try again later.")
+                tk.Canvas.delete(tab1,text_1)
+                tab1.create_text(20,20,anchor="nw",text="try again",font=("Arial,15"))
+                return
             with open("file.txt","w") as f:
                 f.write(desc[0])
                 f.close()
@@ -65,7 +74,7 @@ def start():
                 ind+=1
             ff.close()
         del ff
-        Canvas.delete(tab1,text_1)
+        tk.Canvas.delete(tab1,text_1)
 
         f = open("real.txt","r")
         tab1.create_text(20,0,anchor="nw",text=f.read(),font=("Century Schoolbook",15))
@@ -80,7 +89,7 @@ def start():
             return
         
         def tv_win():
-            tv_page = Toplevel(root)
+            tv_page = tk.Toplevel(root)
             tv_page.title("table view")
             df = pd.DataFrame(table)
             tv = ttk.Treeview(tv_page)
@@ -89,21 +98,21 @@ def start():
                 cols.append(i)
             tv['columns']= tuple(cols)
             tv['show'] = 'headings'
-            tv.column('#0', width=0, stretch=NO)
-            tv.column('0', anchor=CENTER, width=80)
-            tv.column('1', anchor=CENTER, width=80)
-            tv.column('2', anchor=CENTER, width=80)
-            tv.column('3', anchor=CENTER, width=80)
-            tv.column('4', anchor=CENTER, width=80)
-            tv.column('5', anchor=CENTER, width=80)
+            tv.column('#0', width=0, stretch=tk.NO)
+            tv.column('0', anchor=tk.CENTER, width=80)
+            tv.column('1', anchor=tk.CENTER, width=80)
+            tv.column('2', anchor=tk.CENTER, width=80)
+            tv.column('3', anchor=tk.CENTER, width=80)
+            tv.column('4', anchor=tk.CENTER, width=80)
+            tv.column('5', anchor=tk.CENTER, width=80)
 
-            tv.heading('#0', text='', anchor=CENTER)
-            tv.heading('0', text=cols[0], anchor=CENTER)
-            tv.heading('1', text=cols[1], anchor=CENTER)
-            tv.heading('2', text=cols[2], anchor=CENTER)
-            tv.heading('3', text=cols[3], anchor=CENTER)
-            tv.heading('4', text=cols[4], anchor=CENTER)
-            tv.heading('5', text=cols[5], anchor=CENTER)
+            tv.heading('#0', text='', anchor=tk.CENTER)
+            tv.heading('0', text=cols[0], anchor=tk.CENTER)
+            tv.heading('1', text=cols[1], anchor=tk.CENTER)
+            tv.heading('2', text=cols[2], anchor=tk.CENTER)
+            tv.heading('3', text=cols[3], anchor=tk.CENTER)
+            tv.heading('4', text=cols[4], anchor=tk.CENTER)
+            tv.heading('5', text=cols[5], anchor=tk.CENTER)
             for i in range(len(df)):
                 t = df.iloc[i].values.tolist()
                 tv.insert('','end',text='l1',values=(t))
@@ -116,14 +125,14 @@ def start():
         news = data['news']
         table = data['table']
         sentiment = data['mean sentiment']
-        Canvas.delete(tab2, text_2)
+        tk.Canvas.delete(tab2, text_2)
         w = 10
         for i in news:
             tab2.create_text(20,w,text="➡"+i[0] + "  ( "+i[2]+" )",anchor='nw',font=("Century Schoolbook",15))
             w += 40
         tab2.create_text(50,220,anchor="nw",text="News Sentiment Analysis: "+ str(sentiment['Mean Sentiment'][0]),font=("Century Schoolbook",15),fill='red')        
-        tv_btn = Button(tab2,text="click here for full details",command=tv_win,width=20,height =2,font=("Arial",10),border=10,borderwidth=5)
-        tv_btn.pack(pady=80,padx=10,side=LEFT)
+        tv_btn = tk.Button(tab2,text="click here for full details",command=tv_win,width=20,height =2,font=("Arial",10),border=10,borderwidth=5)
+        tv_btn.pack(pady=80,padx=10,side=tk.LEFT)
 
     def stock_prediction():
         tab3.delete('all')
@@ -134,31 +143,34 @@ def start():
         r = r.content
         r = json.loads(r)
 
-        Canvas.delete(tab3,text_3)
+        #check if dictionay is empty
+        if not r:
+            Canvas.delete(tab3,text_3)
+            tab3.create_text(50,10,anchor='nw',text="No data available",font=("Arial",15))
+            return
+
+        tk.Canvas.delete(tab3,text_3)
         text_3 = tab3.create_text(50,10,anchor='nw',text="Loading graphs....",font=("Arial",15))
 
         if "saved_graphs" not in os.listdir():
             os.mkdir("saved_graphs")
-        df = r["prediction"]
-        df = pd.DataFrame(df)
-        fig = ex.line(df,title="Predicted Price")
-        graph_path = os.path.join("saved_graphs","plot.png")
-        fig.write_image(graph_path)
-        img = ImageTk.PhotoImage(Image.open(r"{0}".format(graph_path)))
+        df = pd.DataFrame(r["prediction"])
+        df1  = pd.DataFrame(r["output"])
+        fig = make_subplots(rows=1,cols=2,subplot_titles=("15 days Prediction","Current values with predicted values"))
+        fig.add_trace(go.Scatter(x=df.index,y=df.values.reshape(df.shape[0])),row=1,col=1)
+        fig.add_trace(go.Scatter(x=df1.index,y=df1.values.reshape(df1.shape[0])),row=1,col=2)
+        fig.update_layout(height=600,width=1000)
+        fig.update_xaxes(title_text="Time")
+        fig.update_yaxes(title_text="Prediction")
+        fig.write_image("saved_graphs/"+"plot.png")
+        img = ImageTk.PhotoImage(Image.open("saved_graphs/"+"plot.png"))
+        
+        tk.Canvas.delete(tab3,text_3)
+        
+        tab3.create_image(0,0,image=img,anchor="nw")
 
-        df = pd.DataFrame(r["output"])
-        graph_path = os.path.join("saved_graphs","plot1.png")
-        fig1 = ex.line(df,title="Overall Price")
-        fig.write_image(graph_path)
-        img2 = ImageTk.PhotoImage(Image.open(r"{0}".format(graph_path)))
-
-        Canvas.delete(tab3, text_3)
-        tab3.create_image(0,0,anchor="center",image=img)
-
-        tab3.create_image(0,500,anchor="center",image=img2)
-
-        vbar = Scrollbar(TabControl,orient=VERTICAL)
-        vbar.pack(anchor='e',fill=Y,expand=True)
+        vbar = tk.Scrollbar(TabControl,orient=tk.VERTICAL)
+        vbar.pack(anchor='e',fill='y',expand=True)
         vbar.config(command=tab3.yview)
         tab3.config(yscrollcommand=vbar.set)
         tab3.config(scrollregion=tab3.bbox("all"))
@@ -181,46 +193,46 @@ def restart_program():
     os.execl(python, python, * sys.argv)
 
 
-root = Tk()
+root = tk.Tk()
 root.title("Stock Projection")
 root.geometry('700x400')
 root.config(bg='#429ef5')
 
-var = StringVar()
+var = tk.StringVar()
 
-menubar = Menu(root)
-option = Menu(menubar,tearoff=0)
+menubar = tk.Menu(root)
+option = tk.Menu(menubar,tearoff=0)
 option.add_command(label="Restart",command=restart_program)
 option.add_command(label="Exit",command=root.quit)
 menubar.add_cascade(label="options",menu=option)
 root.config(menu=menubar)
 
-name = Label(root,text="Stock Projection")
+name = tk.Label(root,text="Stock Projection")
 name.configure(font=('Arial',20),fg='Black',bg='lightgrey',width=200)
 name.pack()
 
-label1 = Label(root,text="Enter the stock symbol",border=5)
+label1 = tk.Label(root,text="Enter the stock symbol",border=5)
 label1.pack(side='left',anchor='n',pady=20)
-r1 = Radiobutton(root,text="US",variable=var,value="US")
-r2 = Radiobutton(root,text="IND",variable=var,value="IND")
+r1 = tk.Radiobutton(root,text="US",variable=var,value="US")
+r2 = tk.Radiobutton(root,text="IND",variable=var,value="IND")
 r1.place(x=10,y=100)
 r2.place(x=10,y=130)
-symbol_input = Text(root)
+symbol_input = tk.Text(root)
 symbol_input.config(height=1,width=13,border=3,borderwidth=5)
 symbol_input.place(x=5,y=160)
-btn = Button(root,text='okay',command=start,width=10,border=5,borderwidth=5,bg='lightgreen')
+btn = tk.Button(root,text='okay',command=start,width=10,border=5,borderwidth=5,bg='lightgreen')
 btn.place(x=10,y=190)
 
 TabControl = ttk.Notebook(root)
-tab1 = Canvas(TabControl,bg='lightgrey')
-tab2 = Canvas(TabControl,bg='lightgrey')
-tab3 = Canvas(TabControl,bg='lightgrey')
+tab1 = tk.Canvas(TabControl,bg='lightgrey')
+tab2 = tk.Canvas(TabControl,bg='lightgrey')
+tab3 = tk.Canvas(TabControl,bg='lightgrey')
 
 
 
 TabControl.add(tab1,text='Description')
 TabControl.add(tab2,text='News Sentiment Analysis')
 TabControl.add(tab3,text='Stock Price Forecast')
-TabControl.pack(side=LEFT,expand=True,fill='both')
+TabControl.pack(side=tk.LEFT,expand=True,fill='both')
 
 root.mainloop()
