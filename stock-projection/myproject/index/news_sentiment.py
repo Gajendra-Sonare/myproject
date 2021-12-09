05dd8b06036fb1376b89fea6b57ffcede6b34bd6
Gajendra-Sonare <60471843+Gajendra-Sonare@users.noreply.github.com>
2021-12-09 19:08:13 +0530

    Update news_sentiment.py

--- stock-projection/myproject/index/news_sentiment.py
+++ stock-projection/myproject/index/news_sentiment.py
@@ -1,5 +1,4 @@
-
-# Import libraries
+#Import libraries
 import nltk
 import pandas as pd
 from bs4 import BeautifulSoup
@@ -9,9 +8,8 @@ from nltk.sentiment.vader import SentimentIntensityAnalyzer
 def news_sentiment_analysis(symbol):
     total_data = {}
     # Parameters 
-    n = 3 #the # of article headlines displayed per ticker
+    n = 5 #the # of article headlines displayed per ticker
     tickers = [symbol]
-
     # Get Data
     finwiz_url = 'https://finviz.com/quote.ashx?t='
     news_tables = {}
@@ -27,9 +25,7 @@ def news_sentiment_analysis(symbol):
     try:
         for ticker in tickers:
             df = news_tables[ticker]
-            df_tr = df.findAll('tr')
-            
-        
+            df_tr = df.findAll('tr')    
             #print ('\n')
             #print ('Recent News Headlines for {}: '.format(ticker))
             news = []
@@ -51,16 +47,12 @@ def news_sentiment_analysis(symbol):
         for x in news_table.findAll('tr'):
             text = x.a.get_text() 
             date_scrape = x.td.text.split()
-
             if len(date_scrape) == 1:
-                time = date_scrape[0]
-                
+                time = date_scrape[0]        
             else:
                 date = date_scrape[0]
                 time = date_scrape[1]
-
-            ticker = file_name.split('_')[0]
-            
+            ticker = file_name.split('_')[0]  
             parsed_news.append([ticker, date, time, text])
             
     # Sentiment Analysis
@@ -69,18 +61,14 @@ def news_sentiment_analysis(symbol):
     columns = ['Ticker', 'Date', 'Time', 'Headline']
     news = pd.DataFrame(parsed_news, columns=columns)
     scores = news['Headline'].apply(analyzer.polarity_scores).tolist()
-
     df_scores = pd.DataFrame(scores)
     news = news.join(df_scores, rsuffix='_right')
-
-
     # View Data 
     news['Date'] = pd.to_datetime(news.Date).dt.date
-
     unique_ticker = news['Ticker'].unique().tolist()
     news_dict = {name: news.loc[news['Ticker'] == name] for name in unique_ticker}
-
     values = []
+
     for ticker in tickers: 
         dataframe = news_dict[ticker]
         dataframe = dataframe.set_index('Ticker')
@@ -88,7 +76,6 @@ def news_sentiment_analysis(symbol):
         #print ('\n')
         #print (dataframe.head())
         total_data["table"] = dataframe.head()
-        
         mean = round(dataframe['compound'].mean(), 2)
         values.append(mean)
         
@@ -100,3 +87,5 @@ def news_sentiment_analysis(symbol):
     total_data["mean sentiment"] = df
 
     return total_data
+
+#print(news_sentiment_analysis('tsla')['table'])
